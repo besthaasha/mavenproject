9e3b178128e896f11b13963c6fbc20fa77220c3e
besthaasha <besthaasha746@gmail.com>
2021-11-25 17:23:06 +0530

    first commit in maven

---
+++ src/main/java/ashaaaaaaa/mvntwoproject/mvntwoproject/Application.java
@@ -0,0 +1,9 @@
+package ashaaaaaaa.mvntwoproject.mvntwoproject;
+
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("Hello mvntwoproject");
+    }
+
+}
